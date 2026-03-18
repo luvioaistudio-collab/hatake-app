5177d4eb5405b05f5cc00c95416b583cb79b565e
luvioaistudio-collab <luvioaistudio@gamil.com>
2026-03-18 12:57:33 +0900

    更新

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3237815542" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3831684236" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
