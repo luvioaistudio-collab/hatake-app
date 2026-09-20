a449bdca6ccb33547561dfd2e777e2480f1d365c
luvioaistudio-collab <luvioaistudio@gamil.com>
2026-09-20 16:59:23 +0900

    カオナシ農園を削除

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1261720754" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "315374224" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
